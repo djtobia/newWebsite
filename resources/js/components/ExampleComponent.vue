28508b03664dc62a9c35e8573dcd0f9a8e5b5284
Dylan Tobia <djtobia@gmail.com>
2019-12-30 12:05:53 -0800

    laid out framework for laravel and vuetify

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-app>
+        <v-card>
+            <v-card-title>Title</v-card-title>
+            <v-card-text>Some text</v-card-text>
+            <v-card-actions>
+                <v-btn color="tertiary" @click="changeTheme"> Test</v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-app>
+</template>
+
+<script>
+    export default {
+       data() {
+           return {
+               darkTheme: false
+           };
+       },
+        methods: {
+           changeTheme() {
+               this.darkTheme = !this.darkTheme;
+               this.$vuetify.theme.dark = this.darkTheme;
+           }
+        }
+    }
+</script>
---
+++ resources/plugins/vuetify.js
@@ -0,0 +1,25 @@
+// src/plugins/vuetify.js
+
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib';
+import colors from 'vuetify/lib/util/colors';
+Vue.use(Vuetify);
+
+const opts ={
+    theme: {
+        themes: {
+            light: {
+                primary: colors.green,
+                secondary: colors.red,
+                tertiary: colors.purple
+            },
+            dark: {
+                primary: colors.green.darken2,
+                secondary: colors.red.darken2,
+                tertiary: colors.amber
+            }
+        }
+    }
+};
+
+export default new Vuetify(opts);
